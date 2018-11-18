c64013228826b0984cf8726c80d1908980bd8c89
ErickHartawan <hartawanerick@gmail.com>
2018-11-18 16:54:20 +1100

    just a starting point

--- src/context.js
+++ src/context.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const Context = React.createContext();
+export const Context = React.createContext();
 
 const reducer = (state,action) => {
     switch (action.type){
